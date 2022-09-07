02c968833381cce8a46235b7db6018e0ecd4befc
zweidmitr <zweidmitr@gmail.com>
2022-09-07 21:13:09 +0300

    refactor: ref code

--- project_01/script.js
+++ project_01/script.js
@@ -31,18 +31,10 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score
       document.querySelector('.highscore').textContent = highscore
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!'
-      score--
-      document.querySelector('.score').textContent = score
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!'
-      document.querySelector('.score').textContent = 0
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!'
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high!' : 'Too low!'
       score--
       document.querySelector('.score').textContent = score
     } else {
